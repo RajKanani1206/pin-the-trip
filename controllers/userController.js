9dd61cd03508552a4f0217514be897e54cd128f4
Raj Kanani <rajkanani1206@gmail.com>
2022-12-12 17:12:07 +0530

    Added Logout api

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,3 +40,15 @@ exports.login = BigPromise(async (req, res, next) => {
 
   cookieToken(user, res);
 });
+
+exports.logout = BigPromise(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logout Success",
+  });
+});
