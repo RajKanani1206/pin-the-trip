069b3717d428a53b50d5e73536024f86e825b4c9
Raj Kanani <rajkanani1206@gmail.com>
2022-12-14 18:32:27 +0530

    Added forgot password route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login, logout } = require("../controllers/userController");
+const { register, login, logout, forgotPassword } = require("../controllers/userController");
 
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
+router.route("/forgotPassword").post(forgotPassword);
 
 module.exports = router;
