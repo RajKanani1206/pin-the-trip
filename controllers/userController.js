058c35ecdc38adb2bbabf8902533b7b67303e078
Raj Kanani <rajkanani1206@gmail.com>
2022-12-16 00:06:08 +0530

    Added user details route Api

--- controllers/userController.js
+++ controllers/userController.js
@@ -118,3 +118,12 @@ exports.passwordReset = BigPromise(async (req, res, next) => {
 
   cookieToken(user, res);
 });
+
+exports.getLoggedInUserDetails = BigPromise(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { register, login, logout, forgotPassword, passwordReset } = require("../controllers/userController");
+const {
+  register,
+  login,
+  logout,
+  forgotPassword,
+  passwordReset,
+  getLoggedInUserDetails,
+} = require("../controllers/userController");
+const { isLoggedIn } = require("../middlewares/user");
 
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
 router.route("/forgotPassword").post(forgotPassword);
 router.route("/password/reset/:token").post(passwordReset);
+router.route("/user").get(isLoggedIn, getLoggedInUserDetails);
 
 module.exports = router;
