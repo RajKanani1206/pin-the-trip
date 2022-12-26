d43a58284c66858ecfd06ee2f7f3da3bc232a2d7
Raj Kanani <rajkanani1206@gmail.com>
2022-12-26 18:12:45 +0530

    Refactoring layout changes

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar, Container, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import Logo from "../assets/logo.png";
+import Logo from "../../assets/logo.png";
 import "./style.css";
 
 const Header = () => {
