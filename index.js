b1279694f92dfa1471885a85e29187396a57b837
Raj Kanani <rajkanani1206@gmail.com>
2023-05-17 01:06:54 +0200

    deployment change

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 const connectWithDb = require("./config/db");
+const path = require("path");
 require("dotenv").config();
 
 mongoose.set("strictQuery", true);
@@ -8,6 +9,10 @@ mongoose.set("strictQuery", true);
 // Connect with Database
 connectWithDb();
 
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 app.listen(process.env.PORT || 4000, () => {
   console.log(`Server is running at port ${process.env.PORT}`);
 });
